ab10b3de0514453d79edecdc99ff53eaef7033c2
richalbers <rich.albers@gmail.com>
2025-04-28 12:08:54 -0700

    Update engage2.js

--- engage2.js
+++ engage2.js
@@ -15,7 +15,7 @@
 $(document).ready(function() {
 	//Note:  all functions are defined inside this function 
 	//		 so they all have access to these variables.
-	const googleScriptURL="https://script.google.com/macros/s/AKfycbyM43MEJpqpyxzIlWsbcMuuUdSLe6R4yIRkk27TI8EMlTDWcMQ/exec"
+	const googleScriptURL="https://script.google.com/macros/s/AKfycbwyw0uTqKpPmTG639nf52TH4bPcHRUJ2gYwL8ev0HmF9-a9C6jLd7jwHjp7-4eIf96o/exec"
 
 	//parameters
 	var classSection=getUrlParameter("class");
